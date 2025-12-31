4c37b07d09542b3b3ff39d5adea6c89530721c52
Pradeep Kumar <pradeepkumarhe1989@gmail.com>
2025-12-31 21:16:11 +0530

    util for import and export

---
+++ assets/scripts/util.js
@@ -0,0 +1,9 @@
+/* style-1*/
+export let javascriptDeveloperName="Pradeep";
+
+export let interestedCloudPlatform="AWS";
+
+export let otherHobby="Cricket";
+
+/* style-2*/
+export default "HTML & CSS"
