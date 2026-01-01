5d68e1b8f87ce0063a1960d519e7a917e8c702cd
Pradeep Kumar <pradeepkumarhe1989@gmail.com>
2026-01-01 20:34:15 +0530

    Update app.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+const password=prompt("Enter your password:");
+if(password==="secret123"){
+    alert("Access granted");
+}else{
+    alert("Access denied");
+}
+
+const hobbies=["Software Development","Cricket","organic agriculture"];
+
+for(const hobby of hobbies){
+    console.log(hobby);
+}
